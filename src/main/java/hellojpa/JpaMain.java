9386a63cd6d45ebd6fad864ae8a54fb28e3d09b6
sagara12 <sagara12@naver.com>
2022-06-15 09:03:04 +0900

    2022-06-15 환경 설정 오류 해결 및 JPA 실행

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,41 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            /*
+
+            Member member  = new Member();
+            member.setId(2L);
+            member.setName("HelloB");*/
+
+            /*Member member = em.find(Member.class, 1l);
+            member.setName("HelloJPA");
+
+            em.persist(member);*/
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+            for (Member member: result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+            
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
